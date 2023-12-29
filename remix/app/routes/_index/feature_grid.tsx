5c5d9cf4129a0081d2ebaad1473b13ab83dec64a
Josiah Griggs <josiahstanford8@gmail.com>
2023-12-29 16:36:39 -0500

    Added images

--- remix/app/routes/_index/feature_grid.tsx
+++ remix/app/routes/_index/feature_grid.tsx
@@ -3,26 +3,32 @@ import styles from "@/routes/_index/_index.module.css";
 const cardData = [
   {
     id: 1,
-    title: "Personas",
+    title: "Immersive Conversations",
     content:
-      "Chat from multiple perspectives, lorem ipsum sum lorem ipsum sum lorem",
+      "Step into an enchanting realm of interactive storytelling, " +
+      "where you can engage in deep conversations with AI-driven characters. " +
+      "Flirt, connect, and experience the thrill of building unique bonds with " +
+      "characters who will always be there for you.",
+    source: "/images/image19.jpeg"
+
   },
   {
     id: 2,
-    title: "Immersive Text RPG Experience",
-    content: "Dive into lorem ipsum sum lorem ipsum sum lorem ipsum sum",
+    title: "Image-Chat Fusion",
+    content: "Effortlessly generate vivid images from any chat. "+
+    "Initiate engaging chats from any image. We're the first to offer this seamless integration of "+
+    "image-driven chats and chat-inspired imagery, creating an unparalleled, immersive experience.",
+    source: "/images/image16.jpeg"
   },
   {
     id: 3,
-    title: "Images and Text, All-in-one",
+    title: "Images and Text",
     content:
-      "Create high-quality images of what is happening during a conversaion in real-time lorem ipsum sum lorem ipsum sum",
-  },
-  {
-    id: 4,
-    title: "Create Your Own Chats",
-    content:
-      "Design your own characters to chat with abd add lorem ipsum sum lorem ipsum sum lorem",
+      "Be whoever " + 
+      "you wish to be. Customize your digital avatar, tailoring every aspect to match your desired identity." +
+      "Explore conversations from a new perspective or simply enjoy the thrill of" +
+      "embodying a different persona. Our platform offers the flexibility and freedom to redefine who the AI perceives you as.",
+    source: "/images/image20.jpeg"
   },
 ];
 
@@ -32,8 +38,10 @@ const FeatureGrid = () => {
       {cardData.map((card) => (
         <div key={card.id} className={styles.card_container}>
           <div className={styles.card}>
+            <img src={card.source} alt="" />
             <h2>{card.title}</h2>
             <p>{card.content}</p>
+         
           </div>
         </div>
       ))}

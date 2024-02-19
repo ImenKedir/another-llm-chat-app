690045386c9eee678b285139fc13f5c188bfdb64
Josiah Griggs <josiahstanford8@gmail.com>
2024-02-18 17:36:32 -0800

    commit

--- remix/app/utils/s3.ts
+++ remix/app/utils/s3.ts
@@ -1,7 +1,7 @@
 export function formatS3ImageUrl(
   key: string,
   bucket: string,
-  size: "sm" | "md" | "lg" = "lg",
+  size: "sm" | "md" | "explore" | "lg" = "lg",
 ) {
   if (size === "sm") {
     return `https://${bucket}.s3.us-west-1.amazonaws.com/resized-50w50h-${key}`;
@@ -9,6 +9,10 @@ export function formatS3ImageUrl(
 
   if (size === "md") {
     return `https://${bucket}.s3.us-west-1.amazonaws.com/resized-300w450h-${key}`;
+  }
+
+  if (size === "explore") {
+    return `https://${bucket}.s3.us-west-1.amazonaws.com/resized-400w600h-${key}`;
   }
 
   return `https://${bucket}.s3.us-west-1.amazonaws.com/resized-400w225h-${key}`;
